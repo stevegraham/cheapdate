c3ed4fc57c611c5f38deb56e22ecaa4979432e05
Stevie Graham <sjtgraham@mac.com>
2011-08-29 14:17:28 -0700

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheapdate::Application.config.secret_token = '64ad3860a48cff390a9bb86efcf7e1635fb4be02c60659a89ea4f0dd28d7bd14c0b373b6bf2902ff8a86871a01d842c772cd28a68a061c8797c3dd7fe73139d8'
