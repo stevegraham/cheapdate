57843a6e392e53884d083696be66d34ef74003b0
Stevie Graham <sjtgraham@mac.com>
2011-08-30 12:14:43 -0700

    layout title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #before_filter :authenticate
+  before_filter :authenticate
 
   private
   def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == Settings.user.email && password == Settings.user.password
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == Settings.user.email && password == Settings.user.password
+      end
     end
   end
 end
