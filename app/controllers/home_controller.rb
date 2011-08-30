89630f8bbbb82ec5b302cb93ee72ab06b3a67c23
Stevie Graham <sjtgraham@mac.com>
2011-08-30 15:57:05 -0700

    styling and js to dial a number when you click on the link in the call log

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @capability_token = Twilio::CapabilityToken.create allow_outgoing: Settings.twilio.app_sid
+    @recent_calls     = Twilio::Call.all page_size: 10
   end
 end
